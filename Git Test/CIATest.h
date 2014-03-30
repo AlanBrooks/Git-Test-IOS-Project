03d9994a807f6b393f729f5e15ddffbd9b261975
Alan Brooks <alanbrooks@Dude.local>
2014-03-30 18:04:10 -0400

    added a test class

---
+++ Git Test/CIATest.h
@@ -0,0 +1,13 @@
+//
+//  CIATest.h
+//  Git Test
+//
+//  Created by Alan Brooks on 3/30/14.
+//  Copyright (c) 2014 Chips & Ink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CIATest : NSObject
+
+@end
---
+++ Git Test/CIATest.m
@@ -0,0 +1,13 @@
+//
+//  CIATest.m
+//  Git Test
+//
+//  Created by Alan Brooks on 3/30/14.
+//  Copyright (c) 2014 Chips & Ink. All rights reserved.
+//
+
+#import "CIATest.h"
+
+@implementation CIATest
+
+@end
